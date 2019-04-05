df438add2fb835a465c53df6901c1cef96c6e14c
Makoto Emura <makoto@emurasoft.com>
2019-04-05 14:42:02 -0700

    Initial commit

---
+++ checkerror.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Check(t *testing.T, expectError bool, err error) {
+	if expectError {
+		assert.NotNil(t, err)
+	} else {
+		assert.Nil(t, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/MakotoE/checkerror
+
+go 1.12
+
+require github.com/stretchr/testify v1.3.0
